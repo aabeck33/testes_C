80ee02250a0cfa3726dc3fd2dfb8859c72aa41bd
Alvaro Adriano Beck <111699313+aabeck33@users.noreply.github.com>
2024-05-26 17:16:26 -0300

    Initial commit

---
+++ cs/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+    Comando para iniciar o projeto e, então, iniciar a programação: dotnet new console -o meuPrimeirPrograma
+    See https://aka.ms/new-console-template for more information
+*/
+using System;
+
+namespace HelloWorld
+{
+    class Hello {         
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("The current time is " + DateTime.Now);
+        }
+    }
+}

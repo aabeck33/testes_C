83f8946f9b3d2a0186772e35f2bb6ba76460bafb
Alvaro Adriano Beck <111699313+aabeck33@users.noreply.github.com>
2024-05-26 16:45:16 -0300

    CPP

---
+++ src/TesteCPP.cpp
@@ -0,0 +1,14 @@
+//============================================================================
+// Name        : TesteCPP.cpp
+// Author      : Alvaro Adriano Beck
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+int main() {
+   cout << "Hello, World!" << endl; // This prints Hello, World!
+   return 0;
+}
